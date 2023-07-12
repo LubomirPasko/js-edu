48bf50912787a3cce468b0cbeaab2de66ef3991d
LubomirPasko <101870404+LubomirPasko@users.noreply.github.com>
2023-07-11 20:02:11 -0400

    add

---
+++ HomeAssignments/lesson-9/js/script.js
@@ -0,0 +1,13 @@
+document.write (`Hello, World`);
+
+let a = 'Ivan';
+let b = 64;
+document.write(`Hello ${a}, your age is ${b}`)
+
+/* alert (`hello`); */
+
+a = 3;
+b = 90;
+let sum = a+b;
+console.log(`Sum = ${sum}`);
+console.log();
